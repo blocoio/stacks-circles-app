da846cfff11842f97498b963f5668a6178abe756
FilipeA <filiperamosalves@gmail.com>
2020-12-17 17:50:06 +0000

    added: tests and a way to pollute the strings

--- app/src/main/java/org/stacks/app/data/EncryptedPreferencesIdentityRepository.kt
+++ app/src/main/java/org/stacks/app/data/EncryptedPreferencesIdentityRepository.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import org.stacks.app.data.interfaces.IdentityRepository
+import org.stacks.app.shared.PolluteString
 import org.stacks.app.shared.toFlow
 import timber.log.Timber
 import javax.inject.Inject
@@ -22,7 +23,7 @@ class EncryptedPreferencesIdentityRepository
     private val preferences by lazy { flowSharedPreferences.getString(IDENTITY) }
 
     override suspend fun clear() = withContext(Dispatchers.IO) {
-        preferences.setAndCommit("") //DELETE does not trigger observe
+        preferences.setAndCommit(PolluteString.pollute(preferences.get().length))
     }
 
     override suspend fun set(models: List<IdentityModel>) = withContext(Dispatchers.IO) {
@@ -32,6 +33,7 @@ class EncryptedPreferencesIdentityRepository
     override fun observe(): Flow<List<IdentityModel>> =
         { preferences }
             .toFlow()
+            .filter { !PolluteString.isPolluted(it) }
             .map {
                 return@map toListIdentities(it)
             }
--- app/src/main/java/org/stacks/app/data/EncryptedPreferencesSecretKeyRepository.kt
+++ app/src/main/java/org/stacks/app/data/EncryptedPreferencesSecretKeyRepository.kt
@@ -3,8 +3,10 @@ package org.stacks.app.data
 import com.tfcporciuncula.flow.FlowSharedPreferences
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.withContext
 import org.stacks.app.data.interfaces.SecretKeyRepository
+import org.stacks.app.shared.PolluteString
 import org.stacks.app.shared.toFlow
 import javax.inject.Inject
 import javax.inject.Provider
@@ -20,7 +22,7 @@ class EncryptedPreferencesSecretKeyRepository
     private val preferences by lazy { flowSharedPreferences.getString(SECRET_KEY) }
 
     override suspend fun clear(): Boolean = withContext(Dispatchers.IO) {
-        preferences.setAndCommit("") //DELETE does not trigger observe
+        preferences.setAndCommit(PolluteString.pollute(preferences.get().length))
     }
 
     override suspend fun set(secretKey: String): Boolean = withContext(Dispatchers.IO) {
@@ -30,6 +32,7 @@ class EncryptedPreferencesSecretKeyRepository
     override fun observe(): Flow<String> =
         { preferences }
             .toFlow()
+            .filter { !PolluteString.isPolluted(it) }
 
     companion object {
         const val SECRET_KEY = "secret_key"
