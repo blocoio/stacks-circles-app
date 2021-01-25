6da4d629d62ab8b92192e499bd58d46d0e07efc4
FilipeA <filiperamosalves@gmail.com>
2021-01-25 17:47:35 +0000

    updated: margins and fonts (WIP)

--- app/src/main/java/org/stacks/app/ui/auth/HowItWorksActivity.kt
+++ app/src/main/java/org/stacks/app/ui/auth/HowItWorksActivity.kt
@@ -4,36 +4,30 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
-import kotlinx.android.synthetic.main.activity_connect.*
+import kotlinx.android.synthetic.main.activity_how_it_works.*
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.stacks.app.R
 import org.stacks.app.ui.BaseActivity
-import org.stacks.app.ui.auth.login.LoginActivity
 import reactivecircus.flowbinding.android.view.clicks
 
-class ConnectActivity : BaseActivity() {
+class HowItWorksActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_connect)
+        setContentView(R.layout.activity_how_it_works)
+        setNavigation()
 
-        login
+        getStarted
             .clicks()
             .onEach {
-                startActivity(LoginActivity.getIntent(this))
-            }
-            .launchIn(lifecycleScope)
-
-        howItWorks
-            .clicks()
-            .onEach {
-                startActivity(HowItWorksActivity.getIntent(this))
+                finish()
             }
             .launchIn(lifecycleScope)
     }
 
     companion object {
-        fun getIntent(context: Context) = Intent(context, ConnectActivity::class.java)
+        fun getIntent(context: Context) = Intent(context, HowItWorksActivity::class.java)
     }
+
 }
