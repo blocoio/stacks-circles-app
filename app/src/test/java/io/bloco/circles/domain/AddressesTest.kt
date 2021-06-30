425052d94c9ca7cdf6e65d686e6517680655594c
FilipeA <filiperamosalves@gmail.com>
2021-06-30 14:58:07 +0100

    update: test passing

--- app/src/test/java/io/bloco/circles/domain/AddressesTest.kt
+++ app/src/test/java/io/bloco/circles/domain/AddressesTest.kt
@@ -8,7 +8,7 @@ import me.uport.sdk.core.hexToByteArray
 import org.blockstack.android.sdk.model.BlockstackIdentity
 import org.blockstack.android.sdk.toBtcAddress
 import org.blockstack.android.sdk.toHexPublicKey64
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.kethereum.bip32.generateChildKey
 import org.kethereum.bip32.toKey
@@ -20,15 +20,16 @@ import org.komputing.khash.ripemd160.extensions.digestRipemd160
 import org.komputing.khash.sha256.extensions.sha256
 import org.komputing.khex.extensions.toNoPrefixHexString
 
-@ExperimentalUnsignedTypes
 class AddressesTest {
 
     private val SEED_PHRASE = "float myth tuna chuckle estate recipe canoe equal sport matter zebra vanish pyramid this veteran oppose festival lava economy uniform open zoo shrug fade"
     private val PRIVATE_KEY = "9f6da87aa7a214d484517394ca0689a38faa8b3497bb9bf491bd82c31b5af79601"
     private val BTC_ADDRESS_MAINNET = "1Hu5PUAGWqaokbusF7ZUTpfnejwKbAeGUd"
-    private val BTC_ADDRESS_TESTNET = "mxR2gXFFKs24XiPUxgXrHjt7WjY2Xir3Yn"
-    private val STX_ADDRESS_TESTNET = "ST2WNPKGHNM1PKE1D95KGADR1X5MWXTJHDAYBBZPG"
     private val STX_ADDRESS_MAINNET = "SP2WNPKGHNM1PKE1D95KGADR1X5MWXTJHD8EJ1HHK"
+
+    // Test environment
+    // private val STX_ADDRESS_TESTNET = "ST2WNPKGHNM1PKE1D95KGADR1X5MWXTJHDAYBBZPG"
+    // private val BTC_ADDRESS_TESTNET = "mxR2gXFFKs24XiPUxgXrHjt7WjY2Xir3Yn"
 
     @Test
     fun stxAddressTest() = runBlocking {
@@ -39,16 +40,16 @@ class AddressesTest {
         val sha256 = keys.keyPair.toHexPublicKey64().hexToByteArray().sha256()
         val hash160 = sha256.digestRipemd160()
         val extended = "b0${hash160.toNoPrefixHexString()}"
-        val checksum = checksum(extended)
-        val address = (extended + checksum).hexToByteArray().encodeCrockford32()
+        val cs = checksum("16${hash160.toNoPrefixHexString()}")
+        val address = (extended + cs).hexToByteArray().encodeCrockford32()
 
-        Assert.assertEquals(keys.keyPair.toBtcAddress(), BTC_ADDRESS_MAINNET)
-        Assert.assertEquals(STX_ADDRESS_MAINNET, "S$address")
+        assertEquals(keys.keyPair.toBtcAddress(), BTC_ADDRESS_MAINNET)
+        assertEquals(STX_ADDRESS_MAINNET, "S$address")
     }
 
     private fun checksum(extended: String): String {
         val checksum = extended.hexToByteArray().sha256().sha256()
-        val shortPrefix = checksum.slice(0..4)
+        val shortPrefix = checksum.slice(0..3)
         return shortPrefix.toNoPrefixHexString()
     }
 
