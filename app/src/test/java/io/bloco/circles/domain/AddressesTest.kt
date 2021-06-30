c181f3b8839b4d342dd77e1475d16aff47bd2636
FilipeA <filiperamosalves@gmail.com>
2021-06-30 13:36:37 +0100

    updated: getting closer

--- app/src/test/java/io/bloco/circles/domain/AddressesTest.kt
+++ app/src/test/java/io/bloco/circles/domain/AddressesTest.kt
@@ -1,9 +1,11 @@
 package io.bloco.circles.domain
 
 import io.bloco.circles.shared.decodeCockford32
+import io.bloco.circles.shared.encodeCrockford32
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import me.uport.sdk.core.hexToByteArray
 import org.blockstack.android.sdk.model.BlockstackIdentity
 import org.blockstack.android.sdk.toBtcAddress
 import org.blockstack.android.sdk.toHexPublicKey64
@@ -15,14 +17,17 @@ import org.kethereum.bip39.model.MnemonicWords
 import org.kethereum.bip39.toSeed
 import org.kethereum.extensions.toHexStringNoPrefix
 import org.komputing.kbip44.BIP44Element
-import pm.gnosis.utils.toHex
+import org.komputing.khash.ripemd160.extensions.digestRipemd160
+import org.komputing.khash.sha256.extensions.sha256
+import org.komputing.khex.extensions.toNoPrefixHexString
 
 @ExperimentalUnsignedTypes
 class AddressesTest {
 
     private val SEED_PHRASE = "float myth tuna chuckle estate recipe canoe equal sport matter zebra vanish pyramid this veteran oppose festival lava economy uniform open zoo shrug fade"
     private val PRIVATE_KEY = "9f6da87aa7a214d484517394ca0689a38faa8b3497bb9bf491bd82c31b5af79601"
-    private val BTC_ADDRESS = "mxR2gXFFKs24XiPUxgXrHjt7WjY2Xir3Yn"
+    private val BTC_ADDRESS_MAINNET = "1Hu5PUAGWqaokbusF7ZUTpfnejwKbAeGUd"
+    private val BTC_ADDRESS_TESTNET = "mxR2gXFFKs24XiPUxgXrHjt7WjY2Xir3Yn"
     private val STX_ADDRESS_TESTNET = "ST2WNPKGHNM1PKE1D95KGADR1X5MWXTJHDAYBBZPG"
     private val STX_ADDRESS_MAINNET = "SP2WNPKGHNM1PKE1D95KGADR1X5MWXTJHD8EJ1HHK"
 
@@ -31,12 +36,20 @@ class AddressesTest {
         // Act
         val keys = generateWalletKeysFromMnemonicWords(SEED_PHRASE)
 
-        println("STX ADDRESS: $STX_ADDRESS_MAINNET")
-        println("Decoded STX ADDRESS: ${STX_ADDRESS_MAINNET.decodeCockford32().toHex()}")
-
         // Assert
-        //Assert.assertEquals(keys.keyPair.privateKey.key.toHexStringNoPrefix(), PRIVATE_KEY)
-        Assert.assertEquals(keys.keyPair.toBtcAddress(), BTC_ADDRESS)
+        val sha256 = keys.keyPair.toHexPublicKey64().hexToByteArray().sha256()
+        val hash160 = sha256.digestRipemd160()
+        val extended = "00${hash160.toNoPrefixHexString()}"
+        val noChecksum = extended.hexToByteArray()
+        val address = noChecksum.encodeCrockford32()
+
+        Assert.assertEquals(STX_ADDRESS_MAINNET, "S$address")
+        Assert.assertEquals(keys.keyPair.toBtcAddress(), BTC_ADDRESS_MAINNET)
+    }
+
+    @Test
+    fun decodeTest() {
+        Assert.assertEquals("something", "EDQPTSBMD1MPWSR".decodeCockford32().toNoPrefixHexString())
     }
 
     private suspend fun generateWalletKeysFromMnemonicWords(seedPhrase: String)= withContext(Dispatchers.IO) {
@@ -45,7 +58,7 @@ class AddressesTest {
         val stxKeys = BlockstackIdentity(words.toSeed().toKey("m/44'/5757'/0'/0"))
         //val identity = BlockstackIdentity(words.toSeed().toKey("m/888'/0'"))
 
-        val keys = stxKeys.identityKeys.generateChildKey(BIP44Element(true, 0))
+        val keys = stxKeys.identityKeys.generateChildKey(BIP44Element(false, 0))
 
         val privateKey = keys.keyPair.privateKey.key.toHexStringNoPrefix()
         val publicKey = keys.keyPair.toHexPublicKey64()
